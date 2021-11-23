fa4a33909262234fdb10bb74967b7719ccae8973
unknown <kinchoily@hotmail.com>
2021-11-23 17:44:41 +0000

    NewMeetup - Update handler to send HTTP 'POST' request to our API route

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -2,8 +2,18 @@ import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
 const NewMeetup = () => {
 
-  const addMeetupHandler = (enteredMeetupData) => {
-    console.log(enteredMeetupData);
+  const addMeetupHandler = async (enteredMeetupData) => {
+    //this will send a request to our api file
+    const response = await fetch ('/api/new-meetup', {
+      method: 'POST',
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        'Content-type' : 'application/json'
+      }
+    });
+
+    const data = await response.json();
+    console.log(data);
   };
 
   return (
