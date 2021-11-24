543f523c0798f4f327d839f772af8a4af326f16c
unknown <kinchoily@hotmail.com>
2021-11-24 15:45:23 +0000

    NewMeetup - Add 'head' Metadata

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,3 +1,5 @@
+import { Fragment } from 'react';
+import Head from 'next/head';
 import { useRouter } from 'next/router';
 import NewMeetupForm from '../../components/meetups/NewMeetupForm';
 
@@ -20,7 +22,16 @@ const NewMeetup = () => {
   };
 
   return (
-    <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+    <Fragment>
+      <Head>
+        <title>Add A New Meetup</title>
+        <meta
+          name='description'
+          content='Add and share your own meetup locations and create amazing networking opportunities'
+        />
+      </Head>
+      <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+    </Fragment>
   );
 };
 
